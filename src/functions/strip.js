d9283c20a3a083d679f644d9562905b91a9b658a
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2024-12-28 23:35:13 +0000

    fix strip to actually use template literals 🐾

--- src/functions/strip.js
+++ src/functions/strip.js
@@ -1,5 +1,7 @@
-module.exports = text => text
-   .split(`\n`)
-   .map(text => text.trim())
-   .join(`\n`)
-   .trim();
+module.exports = (text, ...values) =>
+   text
+      .reduce((acc, str, i) => acc + values[i - 1] + str)
+      .trim()
+      .split(`\n`)
+      .map(text => text.trim())
+      .join(`\n`);
