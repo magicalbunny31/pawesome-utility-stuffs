4607f2c4186d1caccb4f41dc2984538e4dd12e34
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2025-03-14 02:10:11 +0000

    force toString() 🐾

--- src/functions/strip.js
+++ src/functions/strip.js
@@ -1,6 +1,6 @@
 module.exports = (text, ...values) =>
    text
-      .reduce((acc, str, i) => acc + values[i - 1] + str)
+      .reduce((acc, str, i) => `${acc}${values[i - 1]}${str}`)
       .trim()
       .split(`\n`)
       .map(text => text.trim())
